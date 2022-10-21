f9a6498f751fb697b71b71dfa7426f0f759b8486
Jose Antonio Ojeda <ti0911342o@gmail.com>
2022-10-21 14:34:54 -0500

    Creación entidada Clientes con JPA

---
+++ spring-boot-data-jpa/src/main/java/com/example/springboot/app/models/entity/Cliente.java
@@ -0,0 +1,41 @@
+package com.example.springboot.app.models.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private static final long serialVersionUID = 1L;
+
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+
+}
