5016d680039ba922d2ad26b068d23d12701f82d3
Jose Antonio Ojeda <ti0911342o@gmail.com>
2022-10-29 17:25:36 -0500

    Se crea script para insertar datos en tabla clientes en DB h2

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+/* SQL SCRIPT CARGA DE DATOS */
+
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (1,'José Antonio','Ojeda','ti0911342o@gmail.com','2022-10-29');
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (2,'José Aarón','Ojeda','aaron@gmail.com','2022-10-29');
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (3,'Viviana','Bonifacio','viviboes@gmail.com','2022-10-29');
+
