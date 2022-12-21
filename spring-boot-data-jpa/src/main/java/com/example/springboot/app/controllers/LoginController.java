c4824ebbe7c5b87d9537fa2b94c8eea93e9e449a
Jose Antonio Ojeda <ti0911342o@gmail.com>
2022-12-21 17:49:05 -0600

    Se agrega validación en LoginController

--- spring-boot-data-jpa/src/main/java/com/example/springboot/app/controllers/LoginController.java
+++ spring-boot-data-jpa/src/main/java/com/example/springboot/app/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.example.springboot.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -11,10 +12,16 @@ import java.security.Principal;
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(Model model, Principal principal, RedirectAttributes flash){
+    public String login(@RequestParam(value = "error",required = false)String error,
+                        Model model,
+                        Principal principal,
+                        RedirectAttributes flash){
         if(principal!=null){
             flash.addFlashAttribute("info","Ya has iniciado sesión anteriormente");
             return "redirect:/cliente/";
+        }
+        if(error!=null){
+            model.addAttribute("error","Operación no permitida: Los datos proporcionados del usuario no existen");
         }
         return "login";
     }
