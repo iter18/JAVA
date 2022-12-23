f459c753efe96c3fc7f655c852e8b84662af86c0
Jose Antonio Ojeda <ti0911342o@gmail.com>
2022-12-22 19:30:05 -0600

    Se agregan entitys de usuario y rol

---
+++ spring-boot-data-jpa/src/main/java/com/example/springboot/app/models/entity/Rol.java
@@ -0,0 +1,26 @@
+package com.example.springboot.app.models.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "authorities",uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id","authority"})})
+public class Rol implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    private String authority;
+
+}
