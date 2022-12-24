e3599ea185fe16db80c46fab1c5c2a2439fc3d90
Jose Antonio Ojeda <ti0911342o@gmail.com>
2022-12-23 18:11:04 -0600

    Se agrega clase para crear vista de PDF

---
+++ spring-boot-data-jpa/src/main/java/com/example/springboot/app/view/pdf/FacturaPdfView.java
@@ -0,0 +1,38 @@
+package com.example.springboot.app.view.pdf;
+
+import com.example.springboot.app.models.entity.Invoice;
+import com.lowagie.text.Document;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractPdfView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Component("factura/ver")
+public class FacturaPdfView extends AbstractPdfView {
+    @Override
+    protected void buildPdfDocument(Map<String, Object> model,
+                                    Document document,
+                                    PdfWriter writer,
+                                    HttpServletRequest request,
+                                    HttpServletResponse response) throws Exception {
+        Invoice factura = (Invoice) model.get("factura");
+
+        PdfPTable tabla = new PdfPTable(1);
+        tabla.addCell("Datos del Cliente");
+        tabla.addCell(factura.getCliente().getNombre()+" "+factura.getCliente().getApellido());
+        tabla.addCell(factura.getCliente().getEmail());
+
+        PdfPTable tabla2 = new PdfPTable(1);
+        tabla2.addCell("Datos de la factura");
+        tabla2.addCell("Folio: "+factura.getId())
+        tabla2.addCell("Descripción: "+factura.getDescripcion());
+        tabla2.addCell("Fecha: "+factura.getCreateAt());
+
+        document.add(tabla);
+        document.add(tabla2)
+    }
+}
