55cd589c8f4a77262d7e492c666fd7e4638b2abd
Jose Antonio Ojeda <ti0911342o@gmail.com>
2023-01-30 17:55:38 -0600

    Se agrega cors a controller para poder acceder desde angular

--- spring-boot-apirest/src/main/java/com/iter/springboot/apirest/controller/ClienteController.java
+++ spring-boot-apirest/src/main/java/com/iter/springboot/apirest/controller/ClienteController.java
@@ -5,12 +5,19 @@ import com.iter.springboot.apirest.modelo.Cliente;
 import com.iter.springboot.apirest.service.ClienteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Se puede configurar los permisos de métodos permitidos al REST, GET,POST,DELETE,etc.
+ * Si se deja sólamente con la dirección del dominio de donde se compartiran los recursos
+ * por defecto tendra permiso a todos los métodos y las cabezeras
+ */
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @Slf4j
 @RequestMapping("/api")
