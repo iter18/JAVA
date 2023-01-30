a919fc83bcb78f5e61de70a13e63f9b20604c7ed
Jose Antonio Ojeda <ti0911342o@gmail.com>
2023-01-30 15:21:57 -0600

    Se agrega entity cliente

---
+++ spring-boot-apirest/src/main/java/com/iter/springboot/apirest/modelo/Cliente.java
@@ -0,0 +1,34 @@
+package com.iter.springboot.apirest.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+}
