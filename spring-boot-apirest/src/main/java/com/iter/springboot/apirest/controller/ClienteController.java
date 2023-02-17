139d6c9d79d31c0bf0e77a26313825c7c7cc96c1
Jose Antonio Ojeda <ti0911342o@gmail.com>
2023-02-17 16:39:01 -0600

    Se cambia método handler a responseEntity para retornar una estructura estandar en respuesta json con errores y tipo de datos

--- spring-boot-apirest/src/main/java/com/iter/springboot/apirest/controller/ClienteController.java
+++ spring-boot-apirest/src/main/java/com/iter/springboot/apirest/controller/ClienteController.java
@@ -5,9 +5,12 @@ import com.iter.springboot.apirest.modelo.Cliente;
 import com.iter.springboot.apirest.service.ClienteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -30,11 +33,31 @@ public class ClienteController {
     }
 
     @GetMapping("/clientes/{id}")
-    public Cliente buscar(@PathVariable Long id){
-        return clienteService.buscar(id);
+    public ResponseEntity<HashMap<String,Object>> buscar(@PathVariable Long id){
+
+        HashMap<String, Object> map = new HashMap<>();
+        try{
+             Cliente cliente = clienteService.buscar(id);
+
+             if(cliente == null){
+                 map.put("mensaje","El cliente que desea buscar no existe!!");
+                 return new ResponseEntity<>(map,HttpStatus.NOT_FOUND);
+             }
+             map.put("reg",cliente);
+             return new ResponseEntity<>(map,HttpStatus.OK);
+        }catch (DataAccessException e){
+            map.put("mensaje", "Error al realizar consulta en DB");
+            map.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<>(map,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        catch(Exception e){
+            map.put("error", e.getMessage());
+            return new ResponseEntity<>(map,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
-    //Este método es una forma haciendo directamente con objetos, pero existe otro método más comercial
+    //Este método es una forma haciendo directamente con objetos, pero existe otro método más comercial para retornar mensajes y objetos
     @PostMapping("/clientes")
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente alta(@RequestBody Cliente cliente){
