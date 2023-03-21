e1820901e630ab7e783818fc1da0d0280005c3f0
Jose Antonio Ojeda <ti0911342o@gmail.com>
2023-03-21 11:35:38 -0600

    Se agregan validaciones para alta de autor y specification

---
+++ spring-boot-apirest/src/main/java/com/iter/springboot/apirest/repository/specification/AutorSpecification.java
@@ -0,0 +1,25 @@
+package com.iter.springboot.apirest.repository.specification;
+
+import com.iter.springboot.apirest.modelo.Autor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.jpa.domain.Specification;
+
+public class AutorSpecification {
+
+    public static Specification<Autor> nombre(String nombre){
+        return (root, query, builder) ->{
+            if(StringUtils.isEmpty(nombre))
+                return builder.conjunction();
+            return builder.equal(builder.upper(root.get("nombre")),nombre.toUpperCase());
+        };
+    }
+
+    public static Specification<Autor> apellido(String apellido){
+        return (root, query, builder) -> {
+            if (StringUtils.isEmpty(apellido))
+                return builder.conjunction();
+            return builder.equal(builder.upper(root.get("apellido")),apellido.toUpperCase());
+        };
+    }
+
+}
--- spring-boot-apirest/src/main/java/com/iter/springboot/apirest/service/impl/AutorServiceImpl.java
+++ spring-boot-apirest/src/main/java/com/iter/springboot/apirest/service/impl/AutorServiceImpl.java
@@ -4,11 +4,15 @@ import com.iter.springboot.apirest.dtos.AutorDto;
 import com.iter.springboot.apirest.mappers.AutorMapper;
 import com.iter.springboot.apirest.modelo.Autor;
 import com.iter.springboot.apirest.repository.AutorRepository;
+import com.iter.springboot.apirest.repository.specification.AutorSpecification;
 import com.iter.springboot.apirest.service.AutorService;
+import io.jsonwebtoken.lang.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AutorServiceImpl implements AutorService {
@@ -19,8 +23,16 @@ public class AutorServiceImpl implements AutorService {
     AutorMapper autorMapper;
     @Override
     public AutorDto alta(AutorDto autorDto) {
-        Autor autor = autorMapper.toEntity(autorDto);
+        Assert.hasText(autorDto.getNombre(),"Campo nombre es requerido");
+        Assert.hasText(autorDto.getApellido(), "Campo apellido es requerido");
 
+        Specification<Autor> filtro = AutorSpecification.nombre(autorDto.getNombre())
+                .and(AutorSpecification.apellido(autorDto.getApellido()));
+        Optional<Autor> registroExiste = autorRepository.findOne(filtro);
+        if(registroExiste.isPresent()){
+           throw new IllegalArgumentException("El registro para este autor ya existe, intente con uno distinto");
+        }
+        Autor autor = autorMapper.toEntity(autorDto);
         return autorMapper.toDto(autorRepository.save(autor));
     }
 
